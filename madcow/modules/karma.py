a96eb578b0c433794cccf40ea124d4a1ba7b551b
Steve Barcomb <stevebarcomb@icloud.com>
2016-01-15 16:10:05 -0500

    karma module has the following changes: - read multiple karma adjustments per line - no longer allows increments to adjustment larger than one - !rank implemented with top 10, and can accept an integer argument to change line output

--- madcow/modules/karma.py
+++ madcow/modules/karma.py
@@ -2,6 +2,7 @@
 
 from madcow.util import Module
 import re
+import pdb
 from learn import Main as Learn
 
 class KarmaResponse(object):
@@ -13,25 +14,35 @@ class KarmaResponse(object):
 
 class Karma(object):
 
-    """Infobot style karma"""
+    """Karma and Ranking"""
 
-    _adjust_pattern = re.compile(r'^\s*(.*?)[+-]([+-]+)\s*$')
+    _adjust_pattern_pos = re.compile(r'\S+(?=\+\+)')
+    _adjust_pattern_neg = re.compile(r'\S+(?=\-\-)')
     _query_pattern = re.compile(r'^\s*karma\s+(\S+)\s*\?*\s*$')
+    _rank_pattern = re.compile(r'^\s*rank(\s+\d+)?')
     _dbname = u'karma'
 
     def __init__(self, madcow):
         self.learn = Learn(madcow)
 
     def process(self, nick, input):
-        kr = KarmaResponse(reply=None, matched=False)
 
+        adjusted = False
+        kr = KarmaResponse(reply='', matched=False)
         # see if someone is trying to adjust karma
         try:
-            target, adjustment = Karma._adjust_pattern.search(input).groups()
-            # don't let people adjust their own karma ;p
-            if nick.lower() != target.lower():
-                self.adjust(nick=target, adjustment=adjustment)
-            kr.matched = True
+            pos = self._adjust_pattern_pos.findall(input)
+            neg = self._adjust_pattern_neg.findall(input)
+            for item in pos:
+                if nick.lower() != item:
+                    adjusted = self.adjust(nick=item, adjustment='++')
+                    karma = self.query(nick=item)
+                    kr.reply += u"%s now has %s points of karma\n" % (item, karma)
+            for item in neg:
+                if nick.lower() != item:
+                    adjusted = self.adjust(nick=item, adjustment='--')
+                    karma = self.query(nick=item)
+                    kr.reply += u"%s now has %s points of karma\n" % (item, karma)
         except AttributeError:
             pass
 
@@ -43,6 +54,17 @@ class Karma(object):
             kr.reply = u"%s: %s's karma is %s" % (nick, target, karma)
         except AttributeError:
             pass
+
+        try:
+            rank_match = Karma._rank_pattern.search(input)
+            number = 10
+            if rank_match.group(1) is not None:
+                number = int(rank_match.groups()[0])
+            keys = self.allkeys()
+            self.orderedKarmaList(keys, number, kr)
+        except AttributeError:
+            pass
+
         return kr
 
     def set(self, nick, karma):
@@ -50,10 +72,36 @@ class Karma(object):
 
     def adjust(self, nick, adjustment):
         karma = self.query(nick)
-        adjustment, size = adjustment[0], len(adjustment)
-        exec(u'karma ' + adjustment + u'= size')
-        self.set(nick=nick, karma=karma)
+        value = None
 
+        if adjustment[:2] == u'++':
+            value = u'+'
+        elif adjustment[:2] == u'--':
+            value = u'-'
+
+        if value:
+            exec(u'karma ' + value + u'= 1')
+            self.set(nick=nick, karma=karma)
+            return True
+
+    def allkeys(self):
+        return self.learn.dbm(Karma._dbname).keys()
+
+    def orderedKarmaList(self, keys, number, kr):
+        rank_list = []
+
+        for key in keys:
+            karma = self.learn.lookup(Karma._dbname, key.lower())
+            rank_list.append((key.lower(), karma))
+
+        rank_list.sort(key=lambda tuple: int(tuple[1]))
+        rank_list.reverse()
+
+        for x in rank_list[:number]:
+            kr.reply += str(x[0]) + " has " + str(x[1]) + " points of karma.\n"
+                
+            
+ 
     def query(self, nick):
         karma = self.learn.lookup(Karma._dbname, nick.lower())
         if karma is None:
@@ -79,6 +127,7 @@ class Main(Module):
         """This function should return a response to the query or None."""
         input = args[0]
         kr = self.karma.process(nick, input)
-        kwargs[u'req'].matched = kr.matched
-        if kr.reply:
+        # Allow us to continue to parse other modules by setting matched equal to False
+        kwargs[u'req'].matched = False
+        if kr.reply is not '':
             return unicode(kr.reply)
